120a21aa5f4176add50494799fef5784197e0d5d
paraglande03 <paraglande03@gmail.com>
2021-04-16 22:48:12 +0530

    Added: Method and mappings for hello message

---
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.myfirstapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello")
+public class HelloWorldController {
+    @RequestMapping(value = {"","/","/home"})
+    public String sayHello(){
+        return "Hello World!!!!";
+    }
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello"+name+" !";
+    }
+}
