9d0724a4a9b2c91f8bfb74b7487bd5dbe322d5cd
paraglande03 <paraglande03@gmail.com>
2021-04-16 22:57:59 +0530

    Refactored: method to get request through query parameters

--- src/main/java/com/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.myfirstapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Hello")
@@ -14,6 +11,10 @@ public class HelloWorldController {
     }
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name){
-        return "Hello"+name+" !";
+        return "Hello "+name+" !";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "hello "+name+" !";
     }
 }
